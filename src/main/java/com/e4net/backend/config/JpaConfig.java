21a96682be664036c66d2538c0099fb019de0cea
Keunoh <rmsdhcjswo1@gmail.com>
2022-08-06 15:51:26 +0900

    08/06 JpaConfig Update

--- src/main/java/com/e4net/backend/config/JpaConfig.java
+++ src/main/java/com/e4net/backend/config/JpaConfig.java
@@ -8,5 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class JpaConfig {
     //단순히 JPA Auditing을 활성화 해주기 위한 클래스
     //BaseEntity를 상속받을 경우 자동으로 시간 생성
+    //git update추가 메세지
 
 }
