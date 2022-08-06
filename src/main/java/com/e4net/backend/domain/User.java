6ea9143ab2a1bc1a26d298ca549b02da12ee6d64
Keunoh <rmsdhcjswo1@gmail.com>
2022-08-06 15:53:32 +0900

    08/06 User Update

--- src/main/java/com/e4net/backend/domain/User.java
+++ src/main/java/com/e4net/backend/domain/User.java
@@ -14,6 +14,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Entity
 public class User extends BaseEntity{
+    //08/06 BaseEntity 상속
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
